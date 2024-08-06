d7444cabb114abf7f86984471d92a0ddc9b7cb66
DevMarg <gurau.margarita@gmail.com>
2024-08-06 12:48:06 +0200

    Implemented login and sign up
    -Added login view allowing users to log in into the app;
    -Added a logout button;
    -Applied form validation and authentication login into the login form;
    -Created sign up view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,8 +2,12 @@ import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+
+  const storedToken = localStorage.getItem("token");
 
   const [movies, setMovies] = useState([]);
 
@@ -11,30 +15,34 @@ export const MainView = () => {
 
   const [user, setUser] = useState(null);
 
-  
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-    fetch("https://movie-spot-a025d6d649af.herokuapp.com/movies")
+    if (!token) return;
+
+    fetch("https://movie-spot-a025d6d649af.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
       .then((movies) => {
         console.log("Fetched data", movies);
 
         const moviesFromApi = movies.map((movie) => {
           return {
-            id: movie._id, 
-            ImageUrl: movie.ImageUrl, 
-            Title: movie.Title, 
-            Description: movie.Description, 
+            id: movie._id,
+            ImageUrl: movie.ImageUrl,
+            Title: movie.Title,
+            Description: movie.Description,
             Genre: {
-              Name: movie.Genre.Name, 
-              Description: movie.Genre.Description 
+              Name: movie.Genre.Name,
+              Description: movie.Genre.Description,
             },
             Director: {
-              Name: movie.Director.Name, 
-              Bio: movie.Director.Bio, 
-              Birthyear: movie.Director.Birthyear, 
-              Deathyear: movie.Director.Deathyear 
-            }
+              Name: movie.Director.Name,
+              Bio: movie.Director.Bio,
+              Birthyear: movie.Director.Birthyear,
+              Deathyear: movie.Director.Deathyear,
+            },
           };
         });
 
@@ -44,14 +52,22 @@ export const MainView = () => {
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-
-  }, []);
+  }, [token]);
 
   if (!user) {
-    return <LoginView />;
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
   }
-
-  
 
   if (selectedMovie) {
     return (
@@ -68,6 +84,15 @@ export const MainView = () => {
 
   return (
     <div>
+      <button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }}
+      >
+        Logout
+      </button>
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
